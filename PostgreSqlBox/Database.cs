14fadcfacb9731a1712b8e9bcefaeb8d1c4bf91d
JohanLarsson <johan_gavle1@hotmail.com>
2018-06-15 17:40:50 +0200

    Use EF.

---
+++ PostgreSqlBox/Database.cs
@@ -0,0 +1,26 @@
+﻿namespace PostgreSqlBox
+{
+    using Microsoft.EntityFrameworkCore;
+    using Npgsql;
+
+    public class Database : DbContext
+    {
+        public Database()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            var connectionStringBuilder = new NpgsqlConnectionStringBuilder
+                          {
+                              Host = "localhost",
+                              Database = "test",
+                          };
+
+            builder.UseNpgsql(connectionStringBuilder.ToString());
+        }
+ 
+        public DbSet<Foo> Foos { get; set; }
+    }
+}
--- PostgreSqlBox/Program.cs
+++ PostgreSqlBox/Program.cs
@@ -1,9 +1,28 @@
 ﻿namespace PostgreSqlBox
 {
+    using System;
+    using System.Diagnostics;
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            const int n = 100_000;
+            using (var db = new Database())
+            {
+                var sw = Stopwatch.StartNew();
+                for (var i = 0; i < n; i++)
+                {
+                    db.Foos.Add(new Foo { Text = "abc" });
+                }
+
+                db.SaveChanges();
+                sw.Stop();
+                Console.WriteLine($"Inserting {n} items took {sw.ElapsedMilliseconds} ms ({sw.ElapsedMilliseconds / n} ms/insert)");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
         }
     }
 }
