c7bdc827b5d3851146afc2469486ca1e0401917f
JohanLarsson <johan_gavle1@hotmail.com>
2018-06-16 12:54:28 +0200

    Format output.

--- PostgreSqlBox/Program.cs
+++ PostgreSqlBox/Program.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.Diagnostics;
+    using System.Linq;
+    using Microsoft.EntityFrameworkCore;
 
     class Program
     {
@@ -11,18 +13,39 @@
             using (var db = new Database())
             {
                 var sw = Stopwatch.StartNew();
-                for (var i = 0; i < n; i++)
-                {
-                    db.Foos.Add(new Foo { Text = "abc" });
-                }
-
+                db.Foos.AddRange(Enumerable.Range(0, n).Select(x => new Foo { Text = x.ToString() }));
                 db.SaveChanges();
                 sw.Stop();
-                Console.WriteLine($"Inserting {n} items took {sw.ElapsedMilliseconds} ms ({sw.ElapsedMilliseconds / n} ms/insert)");
-            }
+                Console.WriteLine($"Inserting {n:N0} items took {sw.ElapsedMilliseconds:N0} ms.");
 
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadLine();
+                sw.Restart();
+                var list = db.Foos.ToList();
+                sw.Stop();
+                Console.WriteLine($"Selecting {list.Count:N0} items took {sw.ElapsedMilliseconds:N0} ms.");
+
+                sw.Restart();
+                var first = db.Foos.First(x => x.Text == "10000");
+                sw.Stop();
+                Console.WriteLine($"Selecting one item took {sw.ElapsedMilliseconds:N0} ms.)");
+
+                sw.Restart();
+                first = db.Foos.First(x => x.Text == "20000");
+                sw.Stop();
+                Console.WriteLine($"Selecting one item took {sw.ElapsedMilliseconds:N0} ms.)");
+
+                sw.Restart();
+                first = db.Foos.First(x => x.Text == "30000");
+                sw.Stop();
+                Console.WriteLine($"Selecting one item took {sw.ElapsedMilliseconds:N0} ms.)");
+
+                sw.Restart();
+                first = db.Foos.First(x => x.Text == "40000");
+                sw.Stop();
+                Console.WriteLine($"Selecting one item took {sw.ElapsedMilliseconds:N0} ms.)");
+
+                db.Database.ExecuteSqlCommand("TRUNCATE \"Foos\" RESTART IDENTITY");
+                db.SaveChanges();
+            }
         }
     }
 }
